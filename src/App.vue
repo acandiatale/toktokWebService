ea0a1355d5079af6bd36c17d9c386295a3b37115
jawsubak <acandiatale@gmail.com>
2020-03-15 21:08:11 +0900

    subList add
    subList add

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,13 @@
         </div>
         <div id="navbar">
             <ul>
-                <li v-for="item in items" :key="item.text">
+                <li id="li_content" v-on:click="getSubList" v-for="item in items" :key="item.text">
                     {{item.text}}
+                    <ul id="subList">
+                        <li v-for="content in items.content" :key="content.text">
+                            {{content.text}}
+                        </li>
+                    </ul>
                 </li>
             </ul>
         </div>
@@ -23,13 +28,20 @@
 <script>
 export default {
   name: 'App',
-  data: ()=>{
-      return{
+  data: function() {
+      return {
         items: [
-            {text: "톡톡경희한의원"},
+            {text: "톡톡경희한의원", content: [{text: "인사말"}, {text: "원장님소개"}]},
             {text: "기본진료"},
-            {text: "공간척추교정"}
+            {text: "공간척추교정"},
+            {text: "턱관절교정"},
+            {text: "치료이야기"},
+            {text: "고객센터"}
         ]
+      }
+  },
+  methods: {
+      getSubList: function() {
 
       }
   }
@@ -60,12 +72,31 @@ html, body {
     flex: 2;
     height: 100%;
 }
+#logo{
+    width: 100%;
+}
 #logo_img{
-    margin-top: 5%;
-    margin-left: 10%;
+    margin-top: 10%;
+    margin-left: 15%;
     width: 167px;
     height: 59px;
     max-height: 5rem;
+}
+#navbar ul{
+    margin-top: 18%;
+    margin-left: 12%;
+}
+#navbar ul li{
+    margin-bottom: 5%;
+    font-weight: bold;
+    margin-right: 10%;
+    font-size: 20px;
+
+}
+#navbar ul li:hover{
+    margin-bottom: 5%;
+    font-weight: bold;
+    color: darksalmon
 }
 #content{
     display: flex;
@@ -84,7 +115,13 @@ html, body {
         display: flex;
         flex-direction: row;
         flex: 2;
-        text-align: center;
+    }
+    #logo_img{
+        margin-top: 3%;
+        margin-left: 7%;
+    }
+    #navbar{
+        display: none;
     }
     /* #nav {
         display: none;
